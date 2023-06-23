0a87818528152b680f9536b20feee2f4a3feaf40
Jingwen Huang <huajingw@sheridancollege.ca>
2023-06-22 21:06:18 -0400

    add comment

---
+++ NetBeansProjects/WarGameGroup02/src/wargamegroup02/Deck.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards;
+
+    //each deck create one cards
+    public Deck(){
+        cards = new ArrayList<>();
+        for(Card.Suit suit: Card.Suit.value()){
+            for(Card.Value value: Card.Value.value()){
+                cards.add(new Card(value, suit));
+            }
+        }
+    }
+
+    //shuffle the cards
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+
+    //player shows the card
+    public Card showCard(){
+        if(cards.isEmpty()){
+            return null;
+        }else{
+            return cards.remove(cards.size() - 1);
+        }
+    }
+}
